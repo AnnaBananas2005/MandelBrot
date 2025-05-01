b16d7fd34842ea47d81dc2f80f063b485f5512f0
Anna Ramirez <129480807+AnnaBananas2005@users.noreply.github.com>
2025-05-01 14:14:23 -0700

    Update ComplexPlane.cpp

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -7,20 +7,23 @@
 #include <sstream>
 #include <complex>
 //Partnered with Anna :3 
+using namespace std;
 
 ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
 {
-    m_pixel_size = (pixelWidth, pixelHeight);
-    m_aspectRatio = (pixelHeight) / pixelWidth;
-    m_plane_center = { 0,0 };
-    m_plane_size = { BASE_WIDTH,BASE_HEIGHT * m_aspectRatio };
+    m_pixel_size = {pixelWidth, pixelHeight};
+    m_aspectRatio = static_cast<float>(pixelHeight) / static_cast<float>(pixelWidth);
+    m_plane_center = {0,0};
+    m_plane_size = {BASE_WIDTH,BASE_HEIGHT * m_aspectRatio};
     m_zoomCount = 0;
-    m_state = STATE::CALCULATING;
+    m_state = State::CALCULATING;
+    m_mouseLocation = {0.f, 0.f};
     //Initialize VertexArray with Points and pixelWidth with pixelHeight
-    m_vArray(Points(pixelWidth * pixelHeight));
+    m_vArray.setPrimitiveType(Points);
+    m_vArray.resize(pixelWidth * pixelHeight );
 }
 
-void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
+void ComplexPlane::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
     target.draw(m_vArray);
 }
@@ -28,83 +31,76 @@ void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
 void ComplexPlane::zoomIn()
 {
     m_zoomCount++;
-    unsigned long long int x = BASE_WIDTH * (pow(BASE_ZOOM, m_ZoomCount));
-    unsigned long long int y = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_ZoomCount));
-    m_plane_size = (x, y);
-    m_state = STATE::CALCULATING;
+    unsigned long long int xNew = BASE_WIDTH * (pow(BASE_ZOOM,m_zoomCount));
+    unsigned long long int yNew = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM,m_zoomCount));
+    m_plane_size = {xNew,yNew};
+    m_state = State::CALCULATING;
 }
 
 void ComplexPlane::zoomOut()
 {
     m_zoomCount--;
-    unsigned long long int x = BASE_WIDTH * (pow(BASE_ZOOM, m_ZoomCount));
-    unsigned long long int y = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_ZoomCount));
-    m_plane_size = (x, y);
-    m_state = STATE::CALCULATING;
+    unsigned long long int xNew = BASE_WIDTH * (pow(BASE_ZOOM,m_zoomCount));
+    unsigned long long int yNew = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM,m_zoomCount));
+    m_plane_size = {xNew,yNew};
+    m_state = State::CALCULATING;
 }
 
-void ComplexPlane::setCenter(vector2i mousePixel)
+void ComplexPlane::setCenter(sf::Vector2i mousePixel)
 {
-    m_plane_center = Vector2f(ComplexPlane::mapPixelToCoords(mousePixel));
-    m_state = STATE::CALCULATING;
+    m_plane_center = sf::Vector2f(ComplexPlane::mapPixelToCoords(mousePixel));
+    m_state = State::CALCULATING;
 }
 
-void ComplexPlane::setMouseLocation(Vector2i mousePixel)
+void ComplexPlane::setMouseLocation(sf::Vector2i mousePixel)
 {
-    m_mouseLocation = Vector2f(COMPLEXPLANE::mapPixelToCoords(mousePixel));
+    m_mouseLocation = sf::Vector2f(ComplexPlane::mapPixelToCoords(mousePixel));
 }
 
-void ComplexPlane::loadText(Text& text)
+void ComplexPlane::loadText(sf::Text& text)
 {
     stringstream words;
 
     words.str("");
 
     words << "Mandelbrot Set" << endl;
-
-    //Will update the x and y coords for the center if button was clicked
-    if (event.mouseButton.button == Mouse::Right || event.mouseButton.button == Mouse::Left)
-    {
-        words << "Center: (" << x << "," << y << ")" << endl;
-    }
-
+    words << "Center: (" << m_plane_center.x << "," << m_plane_center.y << ")" << endl;
     //Will update the cursor when the user moves the mouse in the window screen
-    if (event.type == Event::MouseMoved)
-    {
-        words << "Cursor: (" << x << "," << y << ")" << endl;
-    }
-
+    words << "Cursor: (" << m_mouseLocation.x << "," << m_mouseLocation.y << ")" << endl;
+    
     words << "Left-click to Zoom in" << endl;
     words << "Right-click to Zoom out" << endl;
+
+    text.setString(words.str());
 }
 
 void ComplexPlane::updateRender()
 {
-    if (m_State == CALCULATING)
+    if (m_state == State::CALCULATING)
     {
-        for (int i = 0; i < m_pixel_size.y; i++)
+        for(int j = 0; j < m_pixel_size.x; j++)
         {
-            for (int j = 0; j < m_pixel_size.x; j++)
+            for(int i = 0; i < m_pixel_size.y; i++)
             {
-                Vector2f coord = mapPixelToCoords({ j,i });
-                int iterCount = countIterations(coords);
-                Uint8 r, g, b;
-                iterationsToRGB(itercount, r, g, b);
-                int index = j + i * m_pixel_size.x;
-                m_vArray[j + i * pixelWidth].position = { (float)j,(float)i };
-                m_vArray[j + i * pixelWidth].color = { r,g,b };
+                m_vArray[j + i * m_pixel_size.x].position = {(float)j,(float)i};
+                ComplexPlane::mapPixelToCoords(m_pixel_size);
+                size_t iterCount = ComplexPlane::countIterations(m_plane_size);
+                sf::Uint8 r, g, b;
+                ComplexPlane::iterationsToRGB(iterCount, r, g, b);
+                m_vArray[j + i * m_pixel_size.x].color = { r,g,b };
             }
         }
-        m_state = STATE::DISPLAYING;
+        m_state = State::DISPLAYING;
     }
 }
 
-size_t ComplexPlane::countIterations(Vector2f coord)
+int ComplexPlane::countIterations(sf::Vector2f coord)
 {
+    //Will count the iterations for coord
     size_t iter;
-    for (int i = 0; i < coord.x; i++)
+    for(int i = 0; i < coord.x; i++)
     {
-        for (int j = 0; j < coord.y; j++)
+        for(int j = 0; j <coord.y; j++)
         {
             iter++;
         }
@@ -112,25 +108,61 @@ size_t ComplexPlane::countIterations(Vector2f coord)
     return iter;
 }
 
-void ComplexPlane::iterationsToRGB(size_t count, Unit8& r, Unit8& g, Unit8& b)
+void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
 {
-    r = (0, 255);
-    g = (0, 255);
-    b = (0, 255);
-
-    //set a rabge fron 0 to MAX_ITER either diide tvhe colors to 5 regions
-    //or you can install a sliding effect on the color
+    if(count < 51)
+    {
+        //color number is #04f06a
+        r = 4;
+        g = 241;
+        b = 107;
+    }
+    else if(count >= 51 && count < 102)
+    {
+        //Color number is #abebd2
+        r = 172;
+        g = 236;
+        b = 211;
+    }
+    else if(count >= 102 && count < 153)
+    {
+        //Color number is #9eccbb
+        r = 158;
+        g = 204;
+        b = 187;
+    }
+    else if(count >= 153 && count < 204)
+    {
+        //Color number is #6d5a5a
+        r = 109;
+        g = 90;
+        b = 90;
+    }
+    else if(count >= 204 && count < 255)
+    {
+        //Color number #454545
+        r = 69;
+        g = 69;
+        b = 69;
+    }
+    else
+    {
+        //will make the color black
+        r = 0;
+        g = 0;
+        b = 0;
+    }
 }
 
-Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel)
+sf::Vector2f ComplexPlane::mapPixelToCoords(sf::Vector2i mousePixel)
 {
-    Vector 2f coord;
+    sf::Vector2f newCoord;
 
-    float numX = m_plane_center.x - m_plane_size.x / 2.0;
-    float numY = m_plane_center.y - m_plane_size.y / 2.0;
+    float numX = m_plane_center.x - m_plane_size.x / 2.0f;
+    float numY = m_plane_center.y - m_plane_size.y / 2.0f;
 
-    coord.x = ((mousePixel.x) / m_plane_size.x) * m_plane_size.x + numX;
-    coord.y = ((mousePixel.y) / m_plane_size.y) * m_plane_size.y * numY;
+    newCoord.x = ((mousePixel.x)/m_plane_size.x) * m_plane_size.x + numX;
+    newCoord.y = ((mousePixel.y)/m_plane_size.y) * m_plane_size.y + numY;
 
-    return coord;
+    return newCoord;
 }
