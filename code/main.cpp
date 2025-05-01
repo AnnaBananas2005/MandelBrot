198bcf64f1d915b64734970dc9dc1f5325b91e94
Anna Ramirez <129480807+AnnaBananas2005@users.noreply.github.com>
2025-05-01 14:19:46 -0700

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -4,82 +4,90 @@
 #include <SFML/Graphics/VertexArray.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <iostream>
-//Partnered With Anna :3
-
-using namespace sf;
-using namespace std; 
+#include <sstream>
+#include <complex>
+//Partnered With Anna :3 
+using namespace std;
 
 int main()
 {
-    unsigned int screenWidth = VideoMode::getDesktopMode().width / 2;
-    unsigned int screenHeight = VideoMode::getDesktopMode().height / 2;
+    unsigned int screenWidth = VideoMode::getDesktopMode().width / 2.0;
+    unsigned int screenHeight = VideoMode::getDesktopMode().height / 2.0;
 
-    //Anna did rest of this :3
+    /*class ComplexPlane : public Drawable { //inheritance
+    public:
+        VertexArray Points;
+    private:
+        //put smt here
+    };*/
+    VertexArray vertices(Points);
+
     RenderWindow window(VideoMode(screenWidth, screenHeight), "Complex plane!"); //RenderWindows is required to have string
     ComplexPlane plane(screenWidth, screenHeight);
 
     Font newFont;
-    newFont.loadFromFile("/home/ubuntu/Desktop/cisp400/fonts/Roboto/static/Roboto-Black.ttf"); //put a new path
+    newFont.loadFromFile("./ZillaSlab-Bold.ttf"); //put a new font
 
-    Text newText("MANDELBROT", newFont, 50);
+    //Will be used as  a template for all of the strings
+    Text newText("", newFont, 10);
     newText.setFillColor(Color::White);
     newText.setStyle(Text::Bold);
 
     Event event;
-    while (window.pollEvent(event)){
-        if (event.type == Event::Closed){
-            window.close();
+    while (window.pollEvent(event))
+    {
+        if (event.type == Event::Closed)
+        {
+            window.close(); 
         }
-        if (event.type == Event::MouseButtonPressed){
-            if (event.mouseButton.button == Mouse::Left){
+        if (event.type == Event::MouseButtonPressed)
+        {
+            if (event.mouseButton.button == Mouse::Left)
+            {
                 //Left click will zoomIn and call setCenter on the ComplexPlane object
                 // with the(x, y) pixel location of the mouse click
-                cout << "the left button was pressed" << endl;
-                cout << "mouse x: " << event.mouseButton.x << endl;
-                cout << "mouse y: " << event.mouseButton.y << endl;
+                std::cout << "the left button was pressed" << std::endl;
+                std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+                std::cout << "mouse y: " << event.mouseButton.y << std::endl;
 
                 plane.zoomIn(); 
-                plane.setCenter(event.mouseButton.x, event.mouseButton.y);
+                Vector2i mousePos = Mouse::getPosition();
+                plane.setCenter(mousePos);
             }
             else if (event.mouseButton.button == Mouse::Right) {
             //Right click will zoomOut and call setCenter on the ComplexPlane 
             //object with the (x,y) pixel location of the mouse click
-                cout << "the right button was pressed" << endl;
-                cout << "mouse x: " << event.mouseButton.x << endl;
-                cout << "mouse y: " << event.mouseButton.y << endl;
-
+                std::cout << "the right button was pressed" << std::endl;
+                std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+                std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+                
                 plane.zoomOut();
-                plane.setCenter(event.mouseButton.x, event.mouseButton.y);
-            }   
+                Vector2i mousePs = Mouse::getPosition();
+                plane.setCenter(mousePs);
+
+            }
+            
         }
         else if (event.type == Event::MouseMoved) { 
             //Call setMouseLocation on the ComplexPlane object to store the (x,y) pixel location of the mouse click
             //This will be used later to display the mouse coordinates as it moves
-            plane.setMouseLocation(event.mouseMoved.x, event.mouseMoved.y);
+            Vector2i mouseMo = Mouse::getPosition();
+            plane.setCenter(mouseMo);
         }
         if (Keyboard::isKeyPressed(Keyboard::Escape))
         {
             window.close();
         }
+        //Will call the render to update it and fixing the size
+        plane.updateRender();
+        //Updates the text that has the location of the cursor and the center
+        plane.loadText(newText);
     }
-    /*
-    Update Scene segment
-        Call updateRender on the ComplexPlane object
-        Call loadText on the ComplexPlane object
-    */
-    plane.updateRender();
-    plane.loadText(newText); 
-    /*
-        Draw Scene segment
-        Clear the RenderWindow object
-        draw the ComplexPlane object
-        draw the Text object
-        Display the RenderWindow object
-    */
+    //will draw all the items on the screen and display it for the user to see
     window.clear();
     window.draw(plane);
     window.draw(newText);
     window.display();
-
+    
     return 0;
 }
